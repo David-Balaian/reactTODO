9f9c7f5da11cb5b15720b7ad105bd3f4d3e34af8
David-Balaian <53225473+David-Balaian@users.noreply.github.com>
2019-11-19 02:23:00 +0400

    Updates

---
+++ precache-manifest.283f774aa5900bf148d7a2750b03aebd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13e09022e5ae5cd354f548c0866380d",
+    "url": "/reactTODO/index.html"
+  },
+  {
+    "revision": "d305e7c23c079d3fb3ed",
+    "url": "/reactTODO/static/css/2.9d715279.chunk.css"
+  },
+  {
+    "revision": "d305e7c23c079d3fb3ed",
+    "url": "/reactTODO/static/js/2.7761db9d.chunk.js"
+  },
+  {
+    "revision": "302d24d0cbff153647ff",
+    "url": "/reactTODO/static/js/main.118a0f35.chunk.js"
+  },
+  {
+    "revision": "73fec6167bb02fe6bd90",
+    "url": "/reactTODO/static/js/runtime-main.f966fd5b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactTODO/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactTODO/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactTODO/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactTODO/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactTODO/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
